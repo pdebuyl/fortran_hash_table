f94760d6d303630e1fd3deb77f3da5a2ac6bc64a
Pierre de Buyl <pdebuyl@pdebuyl.be>
2018-03-18 21:02:23 +0100

    do not execute read_write, extend coverage

--- test/read_write.f90
+++ test/read_write.f90
@@ -1,14 +1,41 @@
-program read_write
+program use_ssdm
   use dictionary_m
   implicit none
 
-  type(dictionary_t) :: d
+  type(dictionary_t) :: d, b
+  character(len=128) :: ss, sss
+  integer :: i
 
-  call d%init(1024)
+  call b%init(4*512)
 
-  call d%set('one', 'one')
-  call d%set('pi', '3.14159')
+  do i = 1, 100000
+     read(*, '(a)') ss
+     read(*, '(a)') sss
+     call b%set(trim(ss), trim(sss))
+  end do
 
-  call d%show()
+  call fileshow(b)
 
-end program read_write
+contains
+
+  subroutine fileshow(d)
+    class(dictionary_t), intent(in) :: d
+
+    integer :: i, j, s
+
+    do i = 1, d%dict_size
+       s = d%buckets(i)%current_idx
+       if (s > 0) then
+          do j = 1, s
+             !write(*,*) 'key len', len(d%buckets(i)%entries(j)%key)
+             write(*,'(a)', advance='no') d%buckets(i)%entries(j)%key
+             write(*,'(a)', advance='no') ' = '
+             write(*,'(a)', advance='no') d%buckets(i)%entries(j)%value
+             write(*,*)
+          end do
+       end if
+    end do
+
+  end subroutine fileshow
+
+end program use_ssdm
